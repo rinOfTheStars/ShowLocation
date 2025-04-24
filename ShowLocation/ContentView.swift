6c04524b83575c60b44fcae2e9c205d0d5c09628
Cocona <pexium128@gmail.com>
2025-04-24 19:59:14 -0400

    Initial Commit

---
+++ ShowLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShowLocation
+//
+//  Created by ***REMOVED*** on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowLocation/ShowLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowLocationApp.swift
+//  ShowLocation
+//
+//  Created by ***REMOVED*** on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ShowLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
